bedcab436ea98d56c28e4c8b95b9fa591f156ab5
KERIM KALAFALA <kerimkalafala@KERIMs-MacBook-Pro.local>
2016-02-29 14:48:16 -0500

    Two-way data binding examples.

---
+++ Example1/Example5.js
@@ -0,0 +1,16 @@
+// Credit https://www.youtube.com/watch?v=mW25S2tiCOM&index=4&list=PLexnxRP3h0cocVm7uspMMdUkLhWoj9x-t
+
+var myApp = angular
+	.module("myModule",[])
+	.controller("myController", function($scope){
+		
+	$scope.message = "Hello Angular !";
+	
+	var employee = {
+			firstName: "Kerim",
+			lastName: "Kalafala",
+			gender: "Male"
+		};
+		
+	$scope.employee=employee;
+});
