e885f26c4b7eb61edd77ae93fdd4b2eabb4e14b7
KERIM KALAFALA <kerimkalafala@KERIMs-MacBook-Pro.local>
2016-02-29 14:33:13 -0500

    Illustrate the use of ng-src to defer parsing until after bootstrap process.

---
+++ Example1/Example4.js
@@ -0,0 +1,13 @@
+// Credit https://www.youtube.com/watch?v=mW25S2tiCOM&index=4&list=PLexnxRP3h0cocVm7uspMMdUkLhWoj9x-t
+
+var myApp = angular
+	.module("myModule",[])
+	.controller("myController", function($scope){
+		var employee = {
+			firstName: "Kerim",
+			lastName: "Kalafala",
+			gender: "Male",
+			image: "images/kerim.jpg"
+		};
+	$scope.employee = employee;
+});
