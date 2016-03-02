eafc4118012ec7ed96adc579d61d2249fd71845a
KERIM KALAFALA <kerimkalafala@KERIMs-MacBook-Pro.local>
2016-03-02 09:57:44 -0500

    ng-hide and ng-show examples

---
+++ Example1/Example14.js
@@ -0,0 +1,16 @@
+// Credit https://www.youtube.com/watch?v=MzqkIZLkBsU&index=14&list=PL6n9fhu94yhWKHkcL7RJmmXyxkuFB3KSl
+
+var myApp = angular
+	.module("myModule",[])
+	.controller("myController", function($scope){
+		var employees = [
+			{name: "Kerim", dateOfBirth: new Date("October 20, 1974"), gender: "Male", salary: 50000},
+			{name: "Kristen", dateOfBirth: new Date("September 20, 1976"), gender: "Female", salary: 90000},
+			{name: "Alexis", dateOfBirth: new Date("July 16, 2005"), gender: "Female", salary: 1000.1},
+			{name: "Addison", dateOfBirth: new Date("July 23, 2008"), gender: "Female", salary: 500},
+		];
+		
+		$scope.employees = employees;
+	}
+);
+
